a6b7d72e67b9f531383693919bec2107e8f97fbf
rogelio1502 <rogelio15021515@gmail.com>
2022-08-16 16:49:15 -0500

    images

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: const Center(
-          child: Text(
-            "hola mundo",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.green,
-              backgroundColor: Colors.black,
-            ),
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Image.network(
+            'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*',
+            // height: 250,
+            // width: 350,
+            // fit: BoxFit.fitHeight,
+            fit: BoxFit.contain,
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
